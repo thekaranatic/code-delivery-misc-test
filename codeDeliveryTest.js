f16ad7730d80fdc2bece8fb7820c93e813e0d232
Karan Kakati <karanpkofficial@gmail.com>
2025-01-18 00:23:26 +0530

    Create codeDeliveryTest.js

---
+++ codeDeliveryTest.js
@@ -0,0 +1,16 @@
+function whatsHappening(choice){
+    switch (choice) {
+        case 1:
+            console.log(`You have imported me online through a github repo & your choice is ${choice}`);
+            break;
+        case 2:
+            console.log(`Your choice is ${choice}`);
+            break;
+        case 3:
+            console.log(`Your choice is ${choice} & 3+3=6`);
+            break;
+        case 4:
+            console.log(choice*2);
+            break;
+    }
+}
